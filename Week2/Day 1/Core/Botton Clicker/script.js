38375d2d74ea1fa619d023eed2b3136f75ff1602
Eya Hamdi <eyahamdi184@gmail.com>
2023-05-11 18:51:26 +0100

    Button Clicker

---
+++ Week2/Day 1/Core/Botton Clicker/script.js
@@ -0,0 +1,11 @@
+function loginout(){
+    var btn=document.getElementById("login");
+    btn.innerHTML = "Logout"
+}
+function removebutton(){
+    var button=document.getElementById ("hideButton");
+    button.remove()
+}
+function ninjawasliked(){
+   alert("Ninja was liked")
+}
