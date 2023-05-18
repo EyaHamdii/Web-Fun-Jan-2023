fcb7e76a2de71640486a3b925e7716b71896d0cd
Eya Hamdi <eyahamdi184@gmail.com>
2023-05-18 16:06:25 +0100

    Bankaccount

--- Python/week1/Day2/Core/BankAccount/bankaccount.py
+++ Python/week1/Day2/Core/BankAccount/bankaccount.py
@@ -1,23 +1,34 @@
 class BankAccount:
-    def __init__(self,int_rate,balance=0):
+     def __init__(self,int_rate,balance):
           self.int_rate = int_rate
           self.balance=balance
 
-    def deposit (self ,amount):
+     def deposit (self ,amount):
          self.balance +=amount
+         return self
 
-    def withdraw(self,amount):
-         if self.balance>amount:
-              self.balance-=amount
-         else:  
-            print("Insufficient finds: charging a $5 fee")
-            self.balance-=5
+     def withdraw(self,amount):
+          if self.balance>amount:
+               self.balance-=amount
+          else:  
+               print("Insufficient finds: charging a $5 fee")
+               self.balance-=5
+          return self
     
-    def display_account_info(self):
-     print=(f"balance:${self.balance}")
+     def display_account_info(self):
+          print(f"balance:${self.balance}")
+          return self
     
-    def yield_interest(self):
+     def yield_interest(self):
         if self.balance>0:
-            interest=self.balance*self.int_rate
-            self.balance+= interest
+            self.balance+=(self.balance*self.int_rate)           
+            return self
+account1=BankAccount(0.02,200)
+account1.deposit(300).deposit(100).deposit(100).display_account_info()
+account1.withdraw(150).display_account_info()
+account1.yield_interest().display_account_info()
+account2=BankAccount(0.02,200)
+account2.deposit(150).deposit(150).display_account_info()
+account2.withdraw(50).withdraw(20).withdraw(10).withdraw(5).display_account_info()
+account2.yield_interest().display_account_info()
 
